2ce0f08596076c4d64daf35cb3159e14c328a3f1
Jan Haans <jan.haans@infor.com>
2023-06-01 18:18:47 +0200

    Added assertCorrectMessage helper function

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -7,19 +7,20 @@ func TestHello(t *testing.T) {
 		got := Hello("Jan")
 		want := "Hello, Jan"
 
-		if got != want {
-			t.Errorf("got %q but want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("Say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World"
 
-		if got != want {
-			t.Errorf("got %q but want %q", got, want)
-		}
-
+		assertCorrectMessage(t, got, want)
 	})
+}
 
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q but want %q", got, want)
+	}
 }
