39ba19b4fdce781f47ae7c6b9a6159c88b2f6dbf
Jan Haans <jan.haans@infor.com>
2023-06-01 17:59:11 +0200

    Hello function return 'Hello, World' when empty string supplied

--- hello/hello.go
+++ hello/hello.go
@@ -2,8 +2,13 @@ package main
 
 import "fmt"
 
+const englishHelloPrefix = "Hello,"
+
 func Hello(s string) string {
-	return fmt.Sprintf("Hello %s!", s)
+	if s == "" {
+		return fmt.Sprintf("%s World", englishHelloPrefix)
+	}
+	return fmt.Sprintf("%s %s", englishHelloPrefix, s)
 }
 
 func main() {}
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,10 +3,23 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Jan")
-	want := "Hello Jan!"
+	t.Run("Say hello to people", func(t *testing.T) {
+		got := Hello("Jan")
+		want := "Hello, Jan"
 
-	if got != want {
-		t.Errorf("got %q but want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q but want %q", got, want)
+		}
+	})
+
+	t.Run("Say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		if got != want {
+			t.Errorf("got %q but want %q", got, want)
+		}
+
+	})
+
 }
